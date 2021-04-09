cf99c4288c7a87322d078beac8086755b9fcf7ce
mostafashena <mostafa.shena@gmail.com>
2021-04-10 00:22:12 +0100

    Logic works for vertical and horizontal and only for 7*7

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+var num = 0;
+var counterH = 0;
+var counterV = 0;
+var counterD = 0;
 var player = 0;
 var tileId = 1;
+var currentP = null;
 var player1Img = "1.png";
 var player2Img = "2.png";
 var clicked = false;
@@ -7,7 +12,7 @@ var clicked = false;
 const board = $("#boardGame");
 
 $('.but').click(function (){
-    var num = $(this).text();
+    num = $(this).text();
     $('.but').prop("disabled",true);
     $('.but').css("backgroundColor", "#cccccc");
     $('.but').css("border", "1px solid #999999");
@@ -42,7 +47,11 @@ board.on("click", ".col", function(){
         current.css("backgroundRepeat", "no-repeat");
         current.css("backgroundPosition", "center");
         current.addClass('p1');
-        //hasWin(current);
+        counterH = 0;
+        counterV = 0;
+        counterD = 0;
+        hasWin("p1");
+        currentP = current.attr("class");
         player = 1;
     }else if(player == 1 && !current.hasClass("p1") && !current.hasClass("p2")){
         current.css("backgroundImage", 'url(' + player2Img + ')');
@@ -51,14 +60,64 @@ board.on("click", ".col", function(){
         current.css("backgroundPosition", "center");
         current.addClass('p2');
         current.addClass('selected');
-
-        //hasWin(current);
+        counterH = 0;
+        counterV = 0;
+        counterD = 0;
+        hasWin("p2");
+        currentP = current.attr("class");
         player = 0;
     }
+    //alert(currentP);
 });
 
-    function hasWin(currentTile){
+function hasWin(currentPlayer){
+    var calc = 0;
+    for(var m = 0 ; m < 7; m++){
+        for( var n = 1; n < 5; n++){
+            for(var p=0 ; p<4 ; p++){
+                calc = (7*m) + n + p;
+                //console.log(calc);
+                if($("#"+calc).hasClass(currentPlayer)){
+                    //console.log(calc);
+                    //$("#"+calc).css("backgroundColor", "red");
+                    counterH++;
+                    console.log(calc);
+
+                }
+            }
+            if(counterH==4){
+                alert("you winnnnn");
+                return true;
+            }
+            counterH = 0;
+        }
     }
+    for(var m = 1 ; m < 8; m++){
+        for( var n = 0; n < 4; n++){
+            for(var p=0 ; p<4 ; p++){
+                calc = (7*n) + m + (7*p);
+                //console.log(calc);
+                if($("#"+calc).hasClass(currentPlayer)){
+                    //console.log(calc);
+                    //$("#"+calc).css("backgroundColor", "red");
+                    counterV++;
+                    console.log(calc);
+
+                }
+            }
+            if(counterV==4){
+                alert("you winnnnn");
+                return true;
+            }
+            counterV = 0;
+        }
+    }
+
+
+
+}
+
+//hasWin(p1);
 
 
 
