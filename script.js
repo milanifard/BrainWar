6c6f724824be03ca0954ef71c9805766fa5be423
mostafashena <mostafa.shena@gmail.com>
2021-04-09 17:41:55 +0100

    Your Message

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    if (window.jQuery) {
+        // jQuery is loaded
+        alert("Yeah!");
+    } else {
+        // jQuery is not loaded
+        alert("Doesn't Work");
+    }
+}
