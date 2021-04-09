229811dcd5f16e829788ee7238348c50612b480e
mostafashena <mostafa.shena@gmail.com>
2021-04-09 18:06:00 +0100

    Created and modified board

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-window.onload = function() {
-    if (window.jQuery) {
-        // jQuery is loaded
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
+const board = $("#boardGame");
+
+var row = 7;
+var col = 7;
+var player = null;
+function createBoard(r, c){
+    for(var i = 0; i < r ; i++){
+        const row = $("<div>").addClass("row");
+        for(var j = 0; j < c ; j++){
+            const col = $("<div>").addClass("col");
+            row.append(col);
+        }
+        board.append(row);
     }
 }
+
+createBoard(7,7);
+
+
+$(".col").click(function (){
+    player = 1;
+    var current = $(this);
+    console.log($(this).css('backgroundColor','#333'));
+    if(player == 1){
+        const img = $('<img>').addClass('p1');
+        current.append(img);
+
+    }
+
+});
