3b4b8b80e33a5b8acbf4d194e9937d4839b99f74
mostafashena <mostafa.shena@gmail.com>
2021-04-09 19:46:25 +0100

    Your Message

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const board = $("#boardGame");
 var row = 7;
 var col = 7;
 var player = null;
+var player1Img = "1.png";
+var player2Img = "2.png";
 function createBoard(r, c){
     for(var i = 0; i < r ; i++){
         const row = $("<div>").addClass("row");
@@ -18,13 +20,23 @@ createBoard(7,7);
 
 
 $(".col").click(function (){
-    player = 1;
     var current = $(this);
     console.log($(this).css('backgroundColor','#333'));
-    if(player == 1){
-        const img = $('<img>').addClass('p1');
+    if(player == 0){
+        /*const img = $('<img>').addClass('p1');
         current.append(img);
-        $(".p1").attr("src","1.png")
+        $(".p1").attr("src","1.png");*/
+        current.css("backgroundImage", 'url(' + player1Img + ')');
+        current.css("backgroundSize", "80%");
+        current.css("backgroundRepeat", "no-repeat");
+        current.css("backgroundPosition", "center");
+        player = 1;
+    }else{
+        current.css("backgroundImage", 'url(' + player2Img + ')');
+        current.css("backgroundSize", "80%");
+        current.css("backgroundRepeat", "no-repeat");
+        current.css("backgroundPosition", "center");
+        player = 0;
     }
 
 });
