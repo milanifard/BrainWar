37559d1a43298faaa79ce8d34c7e0d16c4616925
mostafashena <mostafa.shena@gmail.com>
2021-04-09 19:56:46 +0100

    Tiles are implemented and each has an id

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const board = $("#boardGame");
 
 var row = 7;
 var col = 7;
-var player = null;
+var player = 0;
+var tileId = 1;
 var player1Img = "1.png";
 var player2Img = "2.png";
 function createBoard(r, c){
@@ -10,6 +11,8 @@ function createBoard(r, c){
         const row = $("<div>").addClass("row");
         for(var j = 0; j < c ; j++){
             const col = $("<div>").addClass("col");
+            col.attr("id",tileId);
+            tileId++;
             row.append(col);
         }
         board.append(row);
@@ -21,7 +24,6 @@ createBoard(7,7);
 
 $(".col").click(function (){
     var current = $(this);
-    console.log($(this).css('backgroundColor','#333'));
     if(player == 0){
         /*const img = $('<img>').addClass('p1');
         current.append(img);
