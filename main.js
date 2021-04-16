e42498cf9766a68b620f7b094c115812505c36ef
Mohadesekz <52971958+Mohadesekz@users.noreply.github.com>
2021-04-16 21:40:25 +0100

    scroll function working

--- main.js
+++ main.js
@@ -158,9 +158,57 @@ board.on('mouseleave', '.col.hidden', function () {
 
 $(document).keypress(function (e) {
     if (e.which == 102 && hover) {
-        activeCell.css("background-color","#DC3545");
-    }else if (e.which == 114 && hover) {
-        activeCell.css("background-color","#59aec7");
+        activeCell.css("background-color", "#DC3545");
+    } else if (e.which == 114 && hover) {
+        activeCell.css("background-color", "#59aec7");
     }
 });
 
+/* profile js */
+
+var scroll = window.requestAnimationFrame ||
+            function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+function loop() {
+
+  elementsToShow.forEach(function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('is-visible');
+    } else {
+      element.classList.remove('is-visible');
+    }
+  });
+
+  scroll(loop);
+}
+
+loop();
+
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
