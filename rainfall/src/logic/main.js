e8b67f1ab0408efbdf89ce0e8ea9f4adf43882eb
tooraj taraz <tooraj.info@gmail.com>
2021-04-16 10:59:52 +0430

    bug fixing and comments

--- rainfall/src/logic/main.js
+++ rainfall/src/logic/main.js
@@ -22,12 +22,12 @@ let timerInterval;
 
 
 const update_time = (time) => {
-  document.getElementById('curscore').innerHTML = points;
+  document.getElementById('curscore').innerHTML = time === '00:00' ? '00' : points;
   document.getElementById('timepassed').innerHTML = time;
   if(time === '00:10'){
     pause();
     document.getElementById('scores').innerHTML = 'score: ' + points;
-    document.getElementById('accuracy').innerHTML = 'accuracy: ' + (((all - missed) / all) * 100).toFixed(2) + '%';
+    document.getElementById('accuracy').innerHTML = all === 0 ? '0%' : 'accuracy: ' + (((all - missed) / all) * 100).toFixed(2) + '%';
     document.getElementById('showpopup').click();
     reset();
   }
@@ -40,8 +40,6 @@ const start = () => {
     elapsedTime = Date.now() - startTime;
     update_time(timeToString(elapsedTime));
   }, 1000);
-  clearGameBoard();
-  initGameBoard();
   state = true;
   gameLoop();
   showButton("PAUSE");
@@ -54,14 +52,13 @@ const pause = () => {
 }
 
 const reset = () => {
-  const temp = document.getElementById('curscore');
-  temp.innerHTML = '00';
   clearInterval(timerInterval);
   update_time("00:00");
   elapsedTime = 0;
   state = false;
   showButton('PLAY');
   clearGameBoard();
+  initGameBoard();
 }
 
 
@@ -81,34 +78,38 @@ let resetButton = document.getElementById('resetButton');
 * game utils
 */
 const getRandomInt = (min, max) => {
-min = Math.ceil(min);
-max = Math.floor(max);
-return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
 * game logic
 */
 
+/* enums for diffrent positions and element type */
 const posEnum = {"right" : 1, "left" : 2};
 const typeEnum = {"drop" : 1, "lightning" : 2, "nothing" : 3};
 
 Object.freeze(posEnum);
 Object.freeze(typeEnum);
 
-let position = null;
-const gameboard = document.getElementById('gameboard');
-const gameboardCol1 = document.getElementById('gameboardcol1');
-const gameboardCol2 = document.getElementById('gameboardcol2');
-const dropsound = document.getElementById('dropsound');
-const lightningsound = document.getElementById('lightningsound');
-const gameboardCol1Array = [];
-const gameboardCol2Array = [];
+/* needed DOM elements and flags in order to implemenet logic */
+let position = null; /*wheter pointer is left or right*/
+const gameboard = document.getElementById('gameboard'); /* gameboard DIV */
+const gameboardCol1 = document.getElementById('gameboardcol1'); /* left coloumn */
+const gameboardCol2 = document.getElementById('gameboardcol2'); /* right coloumn */
+const dropsound = document.getElementById('dropsound'); /* FX */
+const lightningsound = document.getElementById('lightningsound'); /* FX */
+let gameboardCol1Array = []; /* saved state of left coloumn */
+let gameboardCol2Array = []; /* saved state of right coloumn */
+/* variables for storing score */
 let missed = 0;
 let all = 0;
 let points = 0;
 let state = false;
 
+/* changing lighting for indicating occurence of error */
 const giveWarning = () => {
   gameboard.style.border = '1px solid #ce1212 ';
   gameboard.style.boxShadow = '0 0 0px #c5d7bd, 0 0 50px #ce1212';
@@ -120,22 +121,22 @@ const removeWarning = () => {
 }
 
 
+/* clearing game board for next initialization */
 const clearGameBoard = () => {
-  gameboardCol2.innerHTML = '';
-  gameboardCol1.innerHTML = '';
+  gameboardCol2.innerHTML = "";
+  gameboardCol1.innerHTML = "";
   position = null;
-  for(let i = 0; i < 4; i += 1){
-    try{
-      gameboardCol1Array.pop();
-      gameboardCol2Array.pop();
-    } catch (e){}
-  }
+  gameboardCol1Array = [];
+  gameboardCol2Array = [];
   missed = 0;
   all = 0;
   points = 0;
+  document.onkeydown = null;
 }
 
+/* filling both coloumns in a random fasion */
 const initGameBoard = () => {
+  document.onkeydown = null;
   for(let i = 0; i < 4; i += 1){
     let rand1 = getRandomInt(1, 4);
     let rand2 = getRandomInt(1, 4);
@@ -185,8 +186,9 @@ const initGameBoard = () => {
   }
 };
 
+/* next two functions build modified coloumns after each move */
 const drawGameBoardCol2 = () => {
-  gameboardCol2.innerHTML = ''
+  gameboardCol2.innerHTML = ' '
   for(let i = 0; i < 4; i += 1){
     if(gameboardCol2Array[i] === typeEnum.drop){
       gameboardCol2.innerHTML += `
@@ -210,7 +212,7 @@ const drawGameBoardCol2 = () => {
 }
 
 const drawGameBoardCol1 = () => {
-  gameboardCol1.innerHTML = ''
+  gameboardCol1.innerHTML = ' '
   for(let i = 0; i < 4; i += 1){
     if(gameboardCol1Array[i] === typeEnum.drop){
       gameboardCol1.innerHTML += `
@@ -233,14 +235,14 @@ const drawGameBoardCol1 = () => {
   }
 }
 
+/* main event loop */
 const gameLoop = async () => {
+  document.onkeydown = null;
   for(;;) {
-    if(!state){
-      setTimeout(gameLoop, 200);
-      return;
-    } 
     const moveTo = await waitingKeypress();
+    if(moveTo === -1) return;
     all += 1;
+    /* touching lightning */
     if(gameboardCol1Array[3] === typeEnum.lightning && moveTo === posEnum.left){
       missed += 1;
       points -= 3;
@@ -249,6 +251,7 @@ const gameLoop = async () => {
       setTimeout(removeWarning, 200);
       continue;
     }
+    /* touching lightning */
     if(gameboardCol2Array[3] === typeEnum.lightning && moveTo === posEnum.right){
       missed += 1;
       points -= 3;
@@ -257,6 +260,7 @@ const gameLoop = async () => {
       setTimeout(removeWarning, 200);
       continue;
     }
+    /* missing rain drop */
     if(gameboardCol1Array[3] === typeEnum.drop && moveTo === posEnum.right){
       missed += 1;
       points -= 1;
@@ -264,6 +268,7 @@ const gameLoop = async () => {
       setTimeout(removeWarning, 200);
       continue;
     }
+    /* missing rain drop */
     if(gameboardCol2Array[3] === typeEnum.drop && moveTo === posEnum.left){
       missed += 1;
       points -= 1;
@@ -271,22 +276,27 @@ const gameLoop = async () => {
       setTimeout(removeWarning, 200);
       continue;
     }
+    /* catching rain drops */
     if(gameboardCol1Array[3] === typeEnum.drop && moveTo === posEnum.left){
       points += 5;
       dropsound.cloneNode(true).play();
     }
+    /* catching rain drops */
     if(gameboardCol2Array[3] === typeEnum.drop && moveTo === posEnum.right){
       points += 5;
       dropsound.cloneNode(true).play();
     }
+    /* shifting coloumns down by one elem */
     for(let i = 3; i > 0; i -= 1){
       gameboardCol1Array[i] = gameboardCol1Array[i - 1];
       gameboardCol2Array[i] = gameboardCol2Array[i - 1];
     }
 
+    /* generating new first row (first elemenet of two coloumns) */
     let rand1 = getRandomInt(1, 4);
     let rand2 = getRandomInt(1, 4);
     
+    /* checking for illegal generation */
     while(rand1 === rand2 && (rand1 === typeEnum.lightning || rand1 === typeEnum.drop)){
       rand1 = getRandomInt(1, 4);
       rand2 = getRandomInt(1, 4);
@@ -296,8 +306,8 @@ const gameLoop = async () => {
     drawGameBoardCol1();
     gameboardCol2Array[0] = rand2;
     drawGameBoardCol2();
-    if(!state){
-      setTimeout(gameLoop, 200);
+    /* termination on timeout */
+    if(!state) {
       return;
     }
   }
@@ -307,29 +317,44 @@ const gameLoop = async () => {
  * listeners
  */
 
+/* code of arrow keys */
 const leftArrow = 37;
 const rightArrow = 39;
-playButton.addEventListener('click', start);
-pauseButton.addEventListener('click', pause);
-resetButton.addEventListener('click', reset);
+playButton.addEventListener('click', debounce(start, 300));
+pauseButton.addEventListener('click', debounce(pause, 300));
+resetButton.addEventListener('click', debounce(reset, 300));
 
-function waitingKeypress() {
+const waitingKeypress = () => {
   return new Promise((resolve) => {
     document.addEventListener('keydown', onKeyHandler);
     function onKeyHandler(e) {
-      if (e.keyCode === leftArrow) {
+      /* check for arrow keys being pressed only when state is true, aka, game is being played */
+      if (e.keyCode === leftArrow && state) {
         document.removeEventListener('keydown', onKeyHandler);
         resolve(posEnum.left);
-      } else if (e.keyCode === rightArrow) {
+      } else if (e.keyCode === rightArrow && state) {
         document.removeEventListener('keydown', onKeyHandler);
         resolve(posEnum.right);
+      } else {
+        resolve(-1);
       }
     }
   });
 }
 
+function debounce(callback, wait) {
+  let timerId;
+  return (...args) => {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      callback(...args);
+    }, wait);
+  };
+}
 
 /**
- * function calls
+ * init function calls
  */
 
+ clearGameBoard();
+ initGameBoard();
