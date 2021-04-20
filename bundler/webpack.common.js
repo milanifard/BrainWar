46f7a7af94e2d98695c3a9f634b2e36ad68c89c7
Ali Farabi <farabiali.af@gmail.com>
2021-04-20 21:23:51 +0430

    last update

---
+++ bundler/webpack.common.js
@@ -0,0 +1,97 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+
+module.exports = {
+    entry: path.resolve(__dirname, '../src/script.js'),
+    output:
+    {
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    devtool: 'source-map',
+    plugins:
+    [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, '../static') }
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: path.resolve(__dirname, '../src/index.html'),
+            minify: true
+        }),
+        new HtmlWebpackPlugin({
+            filename: "proGame.html",
+            title: 'pro Game Build with three js',
+            template: path.resolve(__dirname, '../src/proGame.html'),
+            minify: true
+        }),
+        new HtmlWebpackPlugin({
+            filename: "simpleGame.html",
+            title: 'simple Game Build with plain js',
+            template: path.resolve(__dirname, '../src/simpleGame.html'),
+            minify: true
+        }),
+        new MiniCSSExtractPlugin()
+    ],
+    module:
+    {
+        rules:
+        [
+            // HTML
+            {
+                test: /\.(html)$/,
+                use: ['html-loader']
+            },
+
+            // JS
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:
+                [
+                    'babel-loader'
+                ]
+            },
+
+            // CSS
+            {
+                test: /\.css$/,
+                use:["style-loader", "css-loader"]
+            },
+
+            // Images
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                use:
+                [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            outputPath: 'assets/images/'
+                        }
+                    }
+                ]
+            },
+
+            // Fonts
+            {
+                test: /\.(ttf|eot|woff|woff2)$/,
+                use:
+                [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            outputPath: 'assets/fonts/'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
